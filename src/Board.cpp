d884b65853582ce8956f04180c052b89ea26f76f
Shashwat Shukla <shashwatshukla@Shashwats-MacBook-Air.local>
2026-08-13 14:36:55 +0530

    add board state tracking and occupancy caching

--- src/Board.cpp
+++ src/Board.cpp
@@ -22,8 +22,8 @@ void Board::setPiece(int color, int piece, int square)
 void Board::initStartingPosition() 
 {
     /*
-        White Pawns (Rank 2: indices 8-15)
-        Black Pawns (Rank 7: indices 48-55)
+        white pieces are at bottom
+        black pieces at top
     */  
     
     for(int i{}; i < 8; i++) 
@@ -32,47 +32,68 @@ void Board::initStartingPosition()
         setPiece(BLACK, PAWN, 48 + i);
     }
 
-    //Knights
     setPiece(WHITE, KNIGHT, 1); setPiece(WHITE, KNIGHT, 6);
     setPiece(BLACK, KNIGHT, 57); setPiece(BLACK, KNIGHT, 62);
 
-    //Bishops
     setPiece(WHITE, BISHOP, 2); setPiece(WHITE, BISHOP, 5);
     setPiece(BLACK, BISHOP, 58); setPiece(BLACK, BISHOP, 61);
 
-    //Rooks
     setPiece(WHITE, ROOK, 0); setPiece(WHITE, ROOK, 7);
     setPiece(BLACK, ROOK, 56); setPiece(BLACK, ROOK, 63);
 
-    //Queens
     setPiece(WHITE, QUEEN, 3);
     setPiece(BLACK, QUEEN, 59);
 
-    //Kings
     setPiece(WHITE, KING, 4);
     setPiece(BLACK, KING, 60);
+
+    updateOccupancies();
 }
 
 void Board::print() const 
 {
     std::cout << "\n";
-    for (int rank = 7; rank >= 0; rank--) {
-        std::cout << rank + 1 << "  ";
+
+    for(int row{7}; row >= 0; --row) 
+    {
+        std::cout << row + 1 << "  ";
         
-        for (int file = 0; file < 8; file++) {
-            int square = rank * 8 + file;
-            char pieceChar = '.';
+        for(int col{}; col < 8; ++col) 
+        {
+            int square{row * 8 + col};
+            char ch{'.'};
             
-            for (int c = 0; c < 2; c++) {
-                for (int p = 0; p < 6; p++) {
-                    if (get_bit(bitboards[c][p], square)) {
-                        pieceChar = Pieces[c][p];
+            for(int c{}; c < 2; ++c)    
+            {
+                for (int p{}; p < 6; ++p) 
+                {
+                    if(get_bit(bitboards[c][p], square)) 
+                    {
+                        ch = Pieces[c][p];
                     }
                 }
             }
-            std::cout << pieceChar << " ";
+
+            std::cout << ch << " ";
         }
+
         std::cout << "\n";
     }
+
     std::cout << "\n   a b c d e f g h\n\n";
+}
+
+void Board::updateOccupancies() 
+{
+    occupancies[WHITE] = 0;
+    occupancies[BLACK] = 0;
+    occupancies[BOTH]  = 0;
+
+    for(int piece{PAWN}; piece <= KING; ++piece) 
+    {
+        occupancies[WHITE] |= bitboards[WHITE][piece];
+        occupancies[BLACK] |= bitboards[BLACK][piece];
+    }
+
+    occupancies[BOTH] = occupancies[WHITE] | occupancies[BLACK];
 }
--- src/Board.hpp
+++ src/Board.hpp
@@ -6,7 +6,7 @@ using u64 = uint64_t;
 
 enum Color 
 {
-    WHITE, BLACK
+    WHITE, BLACK, BOTH
 };
 
 enum Piece
@@ -19,16 +19,24 @@ class Board
     private:
 
     u64 bitboards[2][6];
+    u64 occupancies[3];
+
+    Color turn;
     const char Pieces[2][6] = 
     {
-        {'P', 'N', 'B', 'R', 'Q', 'K'}, //White Pieces are uppercase
-        {'p', 'n', 'b', 'r', 'q', 'k'}  //Black Pieces are lowercase
+        {'P', 'N', 'B', 'R', 'Q', 'K'}, //White Pieces[0] are uppercase
+        {'p', 'n', 'b', 'r', 'q', 'k'}  //Black Pieces[1] are lowercase
     };
-    
-    public:
 
+    public:
+    
     Board();
     void setPiece(int color, int piece, int square);
     void initStartingPosition();
     void print() const;
+    
+    void updateOccupancies(); 
+    inline u64 getPieceBitboard(int color, int piece) const {return bitboards[color][piece];}
+    inline u64 getOccupancy(int color) const {return occupancies[color];}
+    inline Color getSide() const {return turn;}
 };
